806fd55e8019786edf01c03c9f10b0aa75dea966
Carson Smith <CarsonTSmith@gmail.com>
2024-01-28 18:02:46 -0600

    Adding Chatrooms and working through compiler errors

---
+++ src/core/chatroom.h
@@ -0,0 +1,47 @@
+#ifndef _CHATROOM_H_
+#define _CHATROOM_H_
+
+#include "chatroomclient.h"
+
+#include <functional>
+#include <mutex>
+#include <string>
+#include <unordered_set>
+
+namespace grackle {
+
+class Chatroom {
+
+private:
+    struct ChatroomHash {
+        std::size_t operator()(const Chatroom &chatroom);
+    }; /* struct ChatroomHash*/
+
+
+    std::string m_chatroomName;
+    std::mutex m_chatroomMutex;
+    std::unordered_set<ChatroomClient, ChatroomHash> m_ChatroomClients;
+
+public:
+    Chatroom(const std::string &chatroomName);
+
+    Chatroom(const Chatroom &) = default;
+    Chatroom(Chatroom &&) = delete;
+    Chatroom &operator=(Chatroom &&) = delete;
+    Chatroom &operator=(const Chatroom &) = delete;
+
+    ~Chatroom() = default;
+
+    bool addChatroomClient();
+    bool removeChatroomClient();
+    const std::string &getChatroomName() const;
+
+    friend bool operator==(const Chatroom &lhs, const Chatroom &rhs);
+
+}; /* class Chatroom */
+
+bool operator==(const Chatroom &lhs, const Chatroom &rhs);
+
+} /* namespace grackle */
+
+#endif /* _CHATROOM_H_ */
